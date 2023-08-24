4df6bad81935cd675016a2b5d0b479565e76d052
unknown <macherki.nessrine@yahoo.com>
2023-08-24 16:57:35 +0100

    add to do list app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './components/TodoList'; // Import the new component
+import './App.css';
+
+const App = () => {
+  return (
+
+      <div className="App">
+        < TodoList />
+      </div>
+  );
+};
+
+export default App;
