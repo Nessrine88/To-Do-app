9d1b683c4c5c87e472130224035f967327c8655e
unknown <macherki.nessrine@yahoo.com>
2023-08-24 16:44:01 +0100

    set up linters and packages AND README FILE

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  return (
+    
+      <TodoItem />
+    
+  );
+}
+
+export default App;
